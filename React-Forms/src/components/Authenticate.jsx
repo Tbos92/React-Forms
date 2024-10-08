21abaf81c977512838ae8ba4fa709387b9ffe73a
Tyler <tbos92@gmail.com>
2024-10-08 16:34:09 -0400

    added password length validation and error handling/message for this to alert user. Added CSS styling using glass morphism technique on the signUp and authenticate cards

--- React-Forms/src/components/Authenticate.jsx
+++ React-Forms/src/components/Authenticate.jsx
@@ -1,3 +1,35 @@
-export default function Authenticate() {
-  return <h2>Authenticate!</h2>;
+import { useState } from "react";
+
+export default function Authenticate({ token }) {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      console.log("Authentication Result: ", result);
+      setSuccessMessage(result.message);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div className="card" id="#authenticateCard">
+      <h2>Authenticate!</h2>
+      {successMessage && <p id="success">{successMessage}</p>}
+      {error && <p id="error">{error}</p>}
+      <button className="button" id="authenticateButton" onClick={handleClick}>Authenticate Token</button>
+    </div>
+  );
 }
--- React-Forms/src/components/SignUpForm.jsx
+++ React-Forms/src/components/SignUpForm.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ token, setToken }) {
   // establish variables to be used for handling username, password, and errors
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
   async function handleSubmit(event) {
     // invoke preventDefault to prevent form refresh
     event.preventDefault();
     console.log("Hello");
+
+    // set minimum password length to 8, checking if the entered password is <8 and if true return error message and close out returning 1 for error
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long.");
+      setPassword("");
+      return 1;
+    }
     // send username and password from form to the API using POST
     // use a try catch to fetch response from the API
     try {
@@ -21,10 +29,17 @@ export default function SignUpForm() {
         }
       );
       const result = await response.json();
+      // log result in console to confirm proper object is being returned in response
       console.log(result);
-      // setToken(result.token);
+      setToken(result.token);
+      setSuccessMessage(`Welcome ${username}!`);
+
+      // Clear the form fields
+      setUsername("");
+      setPassword("");
     } catch (error) {
       console.error(error);
+      setError("Error signing up, please try again.");
     }
   }
 
@@ -32,28 +47,35 @@ export default function SignUpForm() {
     // Sign up form with username, password, and submit button functionality to be used to
     // post new user data to the API using above handleSubmit function
     <>
-      <h2>Sign Up!</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            value={username}
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            value={password}
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      <div className="card" id="signUpCard">
+        <h2>Sign Up!</h2>
+        {successMessage && <p id="success">{successMessage}</p>}
+        {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:{" "}
+            <input
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Password:{" "}
+            <input
+              // type="password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+          </label>
+          <button className="button" id="submitButton" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 }
